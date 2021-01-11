b26010aef6fa316f41d046993ec25449534713d7
Pavel Loginov <pavel.loginov.dev@gmail.com>
2021-01-11 17:04:44 +0300

    Modified admin forms (limited categories displaying).

--- shop/main/admin.py
+++ shop/main/admin.py
@@ -6,33 +6,52 @@ from .models import *
 
 
 
-# class GunCategoryChoiceField(forms.ModelChoiceField):
-    # pass
+# TODO: SimpleArrayField(forms.CharField(max_length=100))
+class GunAdmin(admin.ModelAdmin):  
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if (db_field.name == 'category' and 
+                Category.objects.filter(name='Airsoft Guns').exists()):
+            qsc = Category.objects.get(name='Airsoft Guns').get_children()
+            return forms.ModelChoiceField(qsc)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-# class BulletsCategoryChoiceField(forms.ModelChoiceField):
-    # pass
-
-
-#class GunAdmin(admin.ModelAdmin):  
-#SimpleArrayField(forms.CharField(max_length=100))
-
-
-# class BulletsAdmin(admin.ModelAdmin):
+class AmmoAdmin(admin.ModelAdmin):  
     
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        # if db_field.name == 'category':
-            # return BulletsCategoryChoiceField(Category.objects.filter(
-                        # slug='bullets')
-                        # )
-        # return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if (db_field.name == 'category' and 
+                Category.objects.filter(name='BBS & Pellets').exists()):
+            qsc = Category.objects.get(name='BBS & Pellets').get_children()
+            return forms.ModelChoiceField(qsc)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class GearAdmin(admin.ModelAdmin):  
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if (db_field.name == 'category' and 
+                Category.objects.filter(name='Tactical Gear').exists()):
+            qsc = Category.objects.get(name='Tactical Gear').get_children()
+            return forms.ModelChoiceField(qsc)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class AccessoryAdmin(admin.ModelAdmin):  
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if (db_field.name == 'category' and 
+                Category.objects.filter(name='Accessories').exists()):
+            qsc = Category.objects.get(name='Accessories').get_children()
+            return forms.ModelChoiceField(qsc)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(Category)
-admin.site.register(GunItem)#, GunAdmin)
-admin.site.register(AmmoItem)
-admin.site.register(GearItem)
-admin.site.register(AccessoryItem)
+admin.site.register(GunItem, GunAdmin)
+admin.site.register(AmmoItem, AmmoAdmin)
+admin.site.register(GearItem, GearAdmin)
+admin.site.register(AccessoryItem, AccessoryAdmin)
 admin.site.register(CartItem)
 admin.site.register(Cart)
 admin.site.register(Customer)
