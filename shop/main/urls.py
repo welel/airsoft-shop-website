771f4db539c14ed1a55e80f2d0122d03111370a4
Pavel Loginov <pavel.loginov.dev@gmail.com>
2021-01-19 13:27:27 +0300

    View ItemDetailView -> function item_details

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import index, ItemDetailView
+from .views import index, item_details
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('<str:category>/<str:slug>/', ItemDetailView.as_view(),
-            name='item_detail'
-    ),
+    path('<str:category>/<str:slug>/', item_details, name='item_detail'),
 ]
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,35 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
+from django.http import Http404
 
 from .models import Category, LatestItemManager, AmmoItem, GunItem
-
 
 
 def index(request):
     categories = Category.objects.root_nodes()
     items = LatestItemManager.get_last_items()
-    return render(request, 'index.html', {
-                                            'items': items,
-                                            'categories': categories
-                                         }
-    )
-    
-    
-class ItemDetailView(DetailView):
-    
-    CATEGORY_MODEL = {
+    context = {
+        'items': items,
+        'categories': categories
+    }
+    return render(request, 'index.html', context)
+
+
+CATEGORY_MODEL = {
         GunItem.category_parent: GunItem,
         AmmoItem.category_parent: AmmoItem,
-    }
-    
-    def dispatch(self, request, *args, **kwargs):
-        # TODO: OR 404
-        root_category = Category.objects.get(
-                name=kwargs['category']).get_root().name
-        self.model = self.CATEGORY_MODEL[root_category]
-        self.queryset = self.model._base_manager.all()
-        return super().dispatch(request, *args, **kwargs)
-    
-    context_object_name = 'product'
-    template_name = 'single.html'
-    slug_url_kwarg = 'slug'
+}
+
+
+def item_details(request, category, slug):
+    root_category = Category.objects.get(name=category).get_root().name
+    model = CATEGORY_MODEL[root_category]
+    if model:
+        item = get_object_or_404(model, slug=slug)
+    else:
+        raise Http404('Category "{}" does not exist'.format(category))
+    return render(request, 'item_details.html', {'item': item})
